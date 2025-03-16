d794a58f0e3e896b11891b4dd150554581328490
kot <kostia.shiian@gmail.com>
2025-03-16 10:03:10 +0200

    Change cpu configuration

--- jvm-ergonomics/build-run.sh
+++ jvm-ergonomics/build-run.sh
@@ -2,4 +2,4 @@
 
 mvn clean package
 docker build -t jvm-ergonomics .
-docker run --rm --memory=4792MB --cpus="2.0" jvm-ergonomics | grep -Ei "App\:|ergonomic"
+docker run --rm --memory=4792MB --cpus="4.0" jvm-ergonomics | grep -Ei "App\:|ergonomic"
